7ab69258bdb1596baf84c5ba8a3242eca51540bf
Praneet <praneetquantumx@gmail.com>
2020-12-15 00:08:17 +0530

    Add files via upload

--- Project Mendacius/Project_MendaciusApp.swift
+++ Project Mendacius/Project_MendaciusApp.swift
@@ -16,13 +16,17 @@ struct Project_MendaciusApp: App {
             CommandMenu(Text("Downloads"), content: {
                 VStack{
                     Button("initrd - Ubuntu Server", action: {
-                        NSWorkspace.shared.open(URL(string: "https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-amd64-initrd-generic")!)
+                        NSWorkspace.shared.open(URL(string: "https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-\(HostMachine.machineHardwareName == "arm64" ? "arm64" : "amd64")-initrd-generic")!)
                     })
                     Button("vmlinuz - Ubuntu Server", action: {
-                        NSWorkspace.shared.open(URL(string: "https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-amd64-vmlinuz-generic")!)
+                        NSWorkspace.shared.open(URL(string: "https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-\(HostMachine.machineHardwareName == "arm64" ? "arm64" : "amd64")-vmlinuz-generic")!)
                     })
                     Button("cloud image - Ubuntu Server", action: {
-                        NSWorkspace.shared.open(URL(string: "https://drive.google.com/file/d/1RR6cbFwBq4bJh7sgIPByOexXQqdu0Urq/view?usp=sharing")!)
+                        if HostMachine.machineHardwareName == "arm64" {
+                            NSWorkspace.shared.open(URL(string: "https://drive.google.com/file/d/1jRT8Fg85ZI-GpXwhHGNEmy2TUVat5Yjo/view?usp=sharing")!)
+                        } else {
+                            NSWorkspace.shared.open(URL(string: "https://drive.google.com/file/d/1RR6cbFwBq4bJh7sgIPByOexXQqdu0Urq/view?usp=sharing")!)
+                        }
                     })
                 }
             })
